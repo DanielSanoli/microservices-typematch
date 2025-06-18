de3256ef9e25f3c7878848e9f983dd905bd3d72b
danielsanoli <daniel.sanoli28@gmail.com>
2025-06-18 16:14:38 -0300

    Initial commit

---
+++ src/main/java/br/com/typematch/typematch/TypematchApplication.java
@@ -0,0 +1,13 @@
+package br.com.typematch.typematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypematchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/typematch/typematch/TypematchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.typematch.typematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
